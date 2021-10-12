f4f1e51eaeed6359545642128c4cdba20a9a8d01
Berkathia <berkatjayahia99@gmail.com>
2021-10-12 17:03:17 +0700

    push tugas challenge javabasic

---
+++ javabasic.java
@@ -0,0 +1,22 @@
+public class javabasic{
+ 
+    public static void main (String[] args){
+
+
+        String  firstname   = "Berkat jaya ";
+        String  lastname    = "hia";
+        int     age         = 23;
+        String  phone       = "085361453239";
+        String  address     = "petruk joss";
+        char    bloodtype   = 'A';
+        String  hobby       = "Gateball";
+
+        System.out.println("Name         = "+firstname + lastname);
+        System.out.println("Age          = "+age);
+        System.out.println("Number Phone = 08"+phone);
+        System.out.println("Address      = "+address);
+        System.out.println("Blood Type   = "+bloodtype);
+        System.out.println("Hobby        = "+hobby);
+    }
+    
+}
